00da666f87bb13dfb07efb863ddbe336d766552c
Fahad Usmani <fahad.usmani321@gmail.com>
2022-06-03 17:10:51 +0500

    Initial Commit

---
+++ GithubUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubUser
+//
+//  Created by Fahad on 03/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
